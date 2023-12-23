f4296794d25a46352fe0f2741e7f9a8ff09ee670
Ivison Silva <80960148+ivisondsb@users.noreply.github.com>
2023-12-22 23:52:49 -0300

    add selection/view scripts

---
+++ selects.sql
@@ -0,0 +1,16 @@
+# Selecionar todos os clientes
+SELECT * FROM CLIENTES;
+# Visualizar Carros Registrados
+SELECT * FROM CARROS;
+# Visualizar todas as Categorias dos carros
+SELECT * FROM CATEGORIAS;
+# Visualizar Carros Registrados por Cor
+SELECT * FROM CARROS WHERE COR = 'PRETO';
+SELECT * FROM CARROS WHERE COR = 'BRANCO';
+SELECT * FROM CARROS WHERE COR = 'PRATA';
+
+# Selecionar todas as Franquias
+SELECT * FROM FRANQUIAS;
+
+# Selecionar locações
+SELECT * FROM CARLOCA;
